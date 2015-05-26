b69645ef7260564188f7d0c300ef49f2179d904a
Juan B Cabral <jbc.develop@gmail.com>
2015-05-26 10:46:22 -0300

    some things

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,10 +16,17 @@ import sys
 import os
 import shlex
 
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+
+try:
+    import otree
+except ImportError:
+    msg = "Please install 'oTree' from https://github.com/oTree-org/oTree"
+    raise ImportError(msg)
 
 # -- General configuration ------------------------------------------------
 
