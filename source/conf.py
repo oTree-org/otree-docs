75b75e71e4c951c485a9491ce0924d330fd1f77d
Juan B Cabral <jbc.develop@gmail.com>
2015-07-20 20:07:17 -0300

    letter paper to a4

--- source/conf.py
+++ source/conf.py
@@ -249,7 +249,7 @@ htmlhelp_basename = 'oTreedoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
